671e3807b88987d74d51e2397cd6f5587ebca4d0
Ingus Kruklitis <Ingus@Inguss-iMac.local>
2017-07-26 23:40:15 +0300

    added footer

--- src/app/routes/home/index.jsx
+++ src/app/routes/home/index.jsx
@@ -7,6 +7,11 @@ import style from './styles.scss';
 
 export default class Posts extends React.PureComponent {
 
+  constructor(...args) {
+    super(...args);
+    this.getValue = this.getValue.bind(this);
+  }
+
   state = {
     posts: [],
   }
@@ -18,6 +23,10 @@ export default class Posts extends React.PureComponent {
     });
   };
 
+  getValue(val) {
+    return this.state.posts[0] && this.state.posts[0][val];
+  }
+
   // https://ingus-info.firebaseio.com/posts.json
 
   render() {
@@ -27,7 +36,7 @@ export default class Posts extends React.PureComponent {
         <h1><span>Hey there!</span><br/>Welcome to my personal web page where I share my <b>travel</b>, <b>coding</b> and <b>photography</b> experience! <br/> <i>Why don't you start with my latest post?</i></h1>
         <div className="top-slider">
           <div className="slider-description">
-            <h3>{this.state.posts[0] && this.state.posts[0].TravelDate}</h3>
+            <h3>{this.getValue('TravelDate')}</h3>
             <Link to=''><h2>India with<br/>Beautiful Destinations</h2></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
@@ -36,53 +45,54 @@ export default class Posts extends React.PureComponent {
             <Link to=''><img src="../img/sample.jpg" alt="Travel photo" /></Link>
           </div>
         </div>
+        <h2>Some of my latest posts</h2>
         <div className="posts">
           <div className="post-1">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="post-2">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="post-3">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="post-4">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="post-5">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="post-6">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <div className="post-7">
             <Link to=''><img src="../img/sample2.jpg" alt="Travel photo" /></Link>
             <h3>January 21, 2017</h3>
-            <Link to=''><h2>Dubai</h2></Link>
+            <Link to=''><h4>Dubai</h4></Link>
             <h5>Travel adventures</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
--- src/app/routes/wrapper/footer.jsx
+++ src/app/routes/wrapper/footer.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {database} from "firebase";
+import Facebook from '../../../../img/icons/facebook-icon.svg';
+import Instagram from '../../../../img/icons/instagram-icon.svg';
+import Linkedin from '../../../../img/icons/linkedin-icon.svg';
 
 import styles from './styles/styles-footer.scss';
 
-export default () => (
-  <footer>
-    Footer Content
-  </footer>
-);
+export default class Footer extends React.PureComponent {
+
+  constructor(...args) {
+    super(...args);
+    this.getValue = this.getValue.bind(this);
+  }
+
+  state = {
+    content: [],
+  }
+
+  componentDidMount() {
+    var posts = database().ref('master/');
+    posts.on('value', (data) => {
+      this.setState({ content: data.val() });
+    });
+  };
+
+  getValue(val) {
+    return this.state.posts[0] && this.state.posts[0][val];
+  }
+
+  render() {
+    return (
+      <footer>
+        <ul>
+          <li><a href="https://www.facebook.com/ingus.kruklitis" target="_blank"><Facebook /></a></li>
+          <li><a href="https://www.instagram.com/ingus/" target="_blank"><Instagram /></a></li>
+          <li><a href="https://www.linkedin.com/in/ingusk/" target="_blank"><Linkedin /></a></li>
+        </ul>
+        <p>www.ingus.info © All rights reserved.</p>
+      </footer>
+    );
+  }
+};
