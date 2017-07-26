d93a685125e9d7f96c9b1bc3495730998d1785da
Ingus Kruklitis <ingus.kruklitis@gmail.com>
2017-07-26 16:45:08 +0300

    added new styles and translations

--- src/app/routes/about/index.jsx
+++ src/app/routes/about/index.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {database} from "firebase";
+
 import style from './styles.scss';
 
-export default () => (
-  <div className="wrapper">
-    <h1>About page!</h1>
-  </div>
-);
+export default class About extends React.PureComponent {
+
+  constructor(...args) {
+    super(...args);
+    this.getValue = this.getValue.bind(this);
+  }
+
+  state = {
+    aboutPageContent: [],
+  }
+
+  componentDidMount() {
+    var aboutPage = database().ref('about/');
+    aboutPage.on('value', (data) => {
+      this.setState({ aboutPageContent: data.val() });
+    });
+  };
+
+  getValue(val) {
+    return this.state.aboutPageContent[0] && this.state.aboutPageContent[0][val];
+  }
+
+  // https://ingus-info.firebaseio.com/posts.json
+
+  render() {
+    console.log(this.getValue('Title'));
+    return (
+      <div className="wrapper">
+        <h1>{this.getValue('Title')}</h1>
+        <h1>{this.getValue('Sub')}</h1>
+      </div>
+    );
+  }
+};
