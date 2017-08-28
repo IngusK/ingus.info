d9b31f68cd680a430af9acd0e2f684265e404e70
Ingus Kruklitis <ingus.kruklitis@gmail.com>
2017-08-28 17:33:52 +0300

    added countries to world map

--- src/app/components/WorldMap.jsx
+++ src/app/components/WorldMap.jsx
@@ -15,43 +15,13 @@ export default class WorldMap extends React.PureComponent {
       },
       travelPageContent: [],
       worlddata: [],
-      cities: [
-        // { name: "Tokyo",          coordinates: [139.6917,35.6895],  population: 37843000, url: "http://ingustravel.com" },
-        // { name: "Jakarta",        coordinates: [106.8650,-6.1751],  population: 30539000 },
-        // { name: "Delhi",          coordinates: [77.1025,28.7041],   population: 24998000 },
-        // { name: "Manila",         coordinates: [120.9842,14.5995],  population: 24123000 },
-        // { name: "Seoul",          coordinates: [126.9780,37.5665],  population: 23480000 },
-        // { name: "Shanghai",       coordinates: [121.4737,31.2304],  population: 23416000 },
-        // { name: "Karachi",        coordinates: [67.0099,24.8615],   population: 22123000 },
-        // { name: "Beijing",        coordinates: [116.4074,39.9042],  population: 21009000 },
-        // { name: "New York",       coordinates: [-74.0059,40.7128],  population: 21009000, url: "http://ingus.info" },
-        // { name: "Guangzhou",      coordinates: [113.2644,23.1291],  population: 20597000 },
-        // { name: "Sao Paulo",      coordinates: [-46.6333,-23.5505], population: 20365000 },
-        // { name: "Mexico City",    coordinates: [-99.1332,19.4326],  population: 20063000 },
-        // { name: "Mumbai",         coordinates: [72.8777,19.0760],   population: 17712000 },
-        // { name: "Osaka",          coordinates: [135.5022,34.6937],  population: 17444000 },
-        // { name: "Moscow",         coordinates: [37.6173,55.7558],   population: 16170000 },
-        // { name: "Dhaka",          coordinates: [90.4125,23.8103],   population: 15669000 },
-        // { name: "Greater Cairo",  coordinates: [31.2357,30.0444],   population: 15600000 },
-        // { name: "Los Angeles",    coordinates: [-118.2437,34.0522], population: 15058000 },
-        // { name: "Bangkok",        coordinates: [100.5018,13.7563],  population: 14998000 },
-        // { name: "Kolkata",        coordinates: [88.3639,22.5726],   population: 14667000 },
-        // { name: "Buenos Aires",   coordinates: [-58.3816,-34.6037], population: 14122000 },
-        // { name: "Tehran",         coordinates: [51.3890,35.6892],   population: 13532000 },
-        // { name: "Istanbul",       coordinates: [28.9784,§41.0082],   population: 13287000 },
-        // { name: "Lagos",          coordinates: [3.3792,6.5244],     population: 13123000 },
-        // { name: "Shenzhen",       coordinates: [114.0579,22.5431],  population: 12084000 },
-        // { name: "Rio de Janeiro", coordinates: [-43.1729,-22.9068], population: 11727000 },
-        // { name: "Kinshasa",       coordinates: [15.2663,-4.4419],   population: 11587000 },
-        // { name: "Tianjin",        coordinates: [117.3616,39.3434],  population: 10920000 },
-        // { name: "Paris",          coordinates: [2.3522,48.8566],    population: 10858000 },
-        // { name: "Lima",           coordinates: [-77.0428,-12.0464], population: 10750000 },
-      ],
+      cities: [],
+      countries: {},
     }
 
     this.handleCountryClick = this.handleCountryClick.bind(this)
     this.setResizer = this.setResizer.bind(this)
-    this.shouldColor = this.shouldColor.bind(this)
+    this.getCountry = this.getCountry.bind(this)
   }
 
   getValue(val, nr) {
@@ -68,15 +38,17 @@ export default class WorldMap extends React.PureComponent {
   }
 
   componentDidMount() {
+    const worlddata = feature(json, json.objects.countries).features;
     this.setState({
-      worlddata: feature(json, json.objects.countries).features,
+      worlddata,
     })
 
     window.addEventListener('resize', this.setResizer);
 
     const travelPage = database().ref('travel/');
     travelPage.on('value', (data) => {
-      this.setState({ cities: data.val() });
+      const value = data.val();
+      this.setState({ cities: value });
     });
   }
 
@@ -96,16 +68,14 @@ export default class WorldMap extends React.PureComponent {
     })
   }
 
-  // shouldColor(id) {
-  //   return this.state.cities.some(city => {
-  //     return city.country_id.toString() === id;
-  //   });
-  // }
-
-  shouldColor(id) {
-    return this.state.cities.filter(city => {
+  getCountry(id) {
+    const filteredData = this.state.cities.filter(city => {
       return city.country_id.toString() === id;
-    }).length;
+    });
+    return {
+      value: filteredData.length,
+      country: filteredData[0] && filteredData[0].country,
+    }
   }
 
   render() {
@@ -116,10 +86,11 @@ export default class WorldMap extends React.PureComponent {
           <g className="countries">
             {
               this.state.worlddata.map((country,i) => {
-                const value = this.shouldColor(country.id);
-                const colorType = value === 1 ? 'rgba(0,0,0,.4)' : (value > 1 ? 'rgba(0,0,0,.6)' : 'rgba(0,0,0,.15)');
+                const filteredData = this.getCountry(country.id);
+                const colorType = filteredData.value === 1 ? 'rgba(0,0,0,.4)' : (filteredData.value > 1 ? 'rgba(0,0,0,.6)' : 'rgba(0,0,0,.15)');
                 return (
                   <path
+                    data-tip={filteredData.country}
                     key={ `path-${ i }` }
                     d={ geoPath().projection(this.projection())(country) }
                     className="country"
