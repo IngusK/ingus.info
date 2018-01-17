23724dc528ce248cbd3a4cc1ba3b2cfa0ff2b262
Ingus Kruklitis <Ingus.Kruklitis@4finance.com>
2018-01-17 14:14:44 +0200

    configuring post page

--- src/app/routes/blog/posts/post/index.jsx
+++ src/app/routes/blog/posts/post/index.jsx
@@ -22,13 +22,14 @@ export default class BlogPost extends React.PureComponent {
   componentDidMount() {
     const { match: { params } } = this.props;
     var blogPage = database().ref('posts/').orderByChild('slug')
-    .equalTo(params.slug).limitToFirst(1); //database().ref(`blog/post/${params.userId}`);
+    .equalTo(params.slug).limitToFirst(1); //database().ref(blog/post/${params.userId});
 
     blogPage.on('value', (data) => {
       console.log("YYYYYY");
-      console.log(data.val());
+      const post = data.val();
+      const elementKey = Object.keys(post)[0];
       console.log("ZZZZZZ");
-      this.setState({ blogPostPageContent: data.val() });
+      this.setState({ blogPostPageContent: post[elementKey] });
     });
   };
 
@@ -37,49 +38,27 @@ export default class BlogPost extends React.PureComponent {
   }
 
   render() {
+    const { blogPostPageContent } = this.state;
     return (
       <div className="blog-post">
         <div className="blog-post-content">
-          <h2>{this.getValue('title', 0)}</h2>
+          <h2>{blogPostPageContent.title}</h2>
         </div>
-        <div className="blog-post-slide">
-          <img
-            alt={this.getValue('alt', 0)}
-            src={this.getValue('image', 0)}
-          />
-        </div>
+        {
+          blogPostPageContent.photo &&
+          <div className="blog-post-slide">
+            <img
+              alt={blogPostPageContent.title}
+              src={blogPostPageContent.photo}
+            />
+          </div>
+        }
         <div className="blog-post-content-wrapper">
           <SocialIcons
             shareUrl = {this.getValue('sharePost', 0)}
           />
           <div className="blog-post-content">
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 0)}} />
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 1)}} />
-            <h3>{this.getValue('subtitles', 2)}</h3>
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 2)}} />
-            <h3>{this.getValue('subtitles', 3)}</h3>
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 3)}} />
-            <h3>{this.getValue('subtitles', 4)}</h3>
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 4)}} />
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 5)}} />
-            <div className="blog-post-photo right">
-              <Image
-                img={this.getValue('image', 2)}
-                alt={this.getValue('alt', 2)}
-              />
-              <h5 dangerouslySetInnerHTML={{__html:this.getValue('image', 3)}} />
-            </div>
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 6)}} />
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 7)}} />
-            <div className="blog-post-photo single">
-              <Image
-                img={this.getValue('image', 4)}
-                alt={this.getValue('alt', 4)}
-              />
-            </div>
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 8)}} />
-            <p dangerouslySetInnerHTML={{__html:this.getValue('text', 9)}} />
-            <h4 dangerouslySetInnerHTML={{__html:this.getValue('quote', 0)}} />
+            <div dangerouslySetInnerHTML={{__html:blogPostPageContent.content}} />
           </div>
           <SocialIconsMobile
             shareUrl = 'http://ingus.info'
