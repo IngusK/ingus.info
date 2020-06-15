97d2afae3af0717eda099048c713ac08ece8f357
Ingus Kruklitis <Ingus@Inguss-iMac.local>
2020-06-16 01:09:21 +0300

    created store page

---
+++ src/app/routes/store/photos/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import DescriptionPage from '../../../components/descr_page/index.jsx';
+import {database} from "firebase";
+import Head from '../../../components/Helmet/Helmet';
+import style from '../styles.scss';
+
+export default class PhotographyAerial extends React.PureComponent {
+
+  state = {
+    photographyPageContent: [],
+  }
+
+  componentDidMount() {
+    var photographyPage = database().ref('photography/');
+    photographyPage.on('value', (data) => {
+      this.setState({ photographyPageContent: data.val() });
+    });
+  };
+
+  render() {
+    const { photographyPageContent } = this.state;
+    const activePath = this.props.location.pathname;
+
+    if (!photographyPageContent.length) {
+      return <div/>
+    }
+
+    // Take the last part of the activePath
+    const sectionName = activePath.split('/').slice(-1).pop();
+    const photoDescr = `${sectionName}-descr`;
+
+    const currentData = photographyPageContent.find(obj => obj.slug === sectionName);
+    const title = currentData.titles;
+    const descr = currentData.descr;
+    return (
+      <div className="photo-content">
+        <Head
+          title={`ingus.info - ${title}`}
+          content={`ingus.info - ${descr}`}
+        />
+        <h2>{title}</h2>
+        <p dangerouslySetInnerHTML={{__html:`${descr}`}} />
+        {photographyPageContent.map((item, index) => (
+          <DescriptionPage
+            key={index}
+            img={photographyPageContent[index][sectionName]}
+            alt={photographyPageContent[index][photoDescr]}
+            descr={photographyPageContent[index][photoDescr]}
+          />
+        ))}
+      </div>
+    );
+  }
+};
